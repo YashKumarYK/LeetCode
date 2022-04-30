187ebf44ea63f8b3860266fad5451afd20d3435a
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-01 03:32:36 +0530

    Time: 18 ms (37.56%), Space: 20.9 MB (7.09%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,24 +11,42 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root ==NULL) return 0;
-        else{
-            int left = height(root->left);
-            int right = height(root-> right);
-            int ans = max(left ,right) +1;
-            return ans;
-        }
+    // int height(TreeNode* root){
+    //     if(root ==NULL) return 0;
+    //     else{
+    //         int left = height(root->left);
+    //         int right = height(root-> right);
+    //         int ans = max(left ,right) +1;
+    //         return ans;
+    //     }
+    // }
+    pair<int,int> fast_dia(TreeNode* root){
+        if( root==NULL) {
+            pair<int, int> p = make_pair(0,0);
+            return p;
+        };
+        pair<int, int> left = fast_dia(root->left);
+        pair<int,int > right = fast_dia(root-> right);
+        
+        int opt1 = left.first;
+        int opt2 = right.first;
+        int opt3 = left.second + right.second ;
+        
+        pair<int, int> ans;
+        ans.first = max( opt1, max(opt2, opt3));
+        ans.second = max(left.second, right.second)+1;
+        return ans;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if( root ==NULL) return 0;
-        else{
-            int opt1 = diameterOfBinaryTree(root->left);
-            int opt2 = diameterOfBinaryTree(root-> right);
-            int opt3 = height( root->left) + height(root->right) ;
-            int ans = max(opt1, max(opt2, opt3));
-            return ans ;
+//         if( root ==NULL) return 0;
+//         else{
+//             int opt1 = diameterOfBinaryTree(root->left);
+//             int opt2 = diameterOfBinaryTree(root-> right);
+//             int opt3 = height( root->left) + height(root->right) ;
+//             int ans = max(opt1, max(opt2, opt3));
+//             return ans ;
             
-        }
+//         }
+        return fast_dia(root).first;
     }
 };

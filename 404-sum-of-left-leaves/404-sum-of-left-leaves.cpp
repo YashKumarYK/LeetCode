3cdfd3d1b25caed97f6b82f09a8edec149a3860a
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-08 05:13:02 +0530

    Time: 10 ms (16.86%), Space: 13.6 MB (10.94%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -11,24 +11,37 @@
  */
 class Solution {
 public:
-    bool isleaf(TreeNode* root){
-        if( root==NULL) return false;
-        if( root->left ==NULL &&  root->right == NULL){
-            return true;
-        }
-        return false;
-    }
+    // bool isleaf(TreeNode* root){
+    //     if( root==NULL) return false;
+    //     if( root->left ==NULL &&  root->right == NULL){
+    //         return true;
+    //     }
+    //     return false;
+    // }
     int sumOfLeftLeaves(TreeNode* root) {
-        if(root== NULL){
-            return 0;
-        }
+//         if(root== NULL){
+//             return 0;
+//         }
         
-        if(isleaf(root->left)){
-            return root->left->val + sumOfLeftLeaves(root->right);
+//         if(isleaf(root->left)){
+//             return root->left->val + sumOfLeftLeaves(root->right);
+//         }
+//         else{
+//             return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+//         }
+        queue<TreeNode*> q;
+        if (root==NULL) return false;
+        q.push(root);
+        int sum = 0;
+        while( !q.empty()){
+            TreeNode* front = q.front();
+            q.pop();
+            if( front->left !=NULL && front->left->left ==NULL && front->left->right ==NULL){
+                sum = sum+ front->left->val;
+            }
+            if( front->left) q.push(front->left);
+            if(front->right) q.push(front->right);
         }
-        else{
-            return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
-        }
-        
+        return sum;
     }
 };

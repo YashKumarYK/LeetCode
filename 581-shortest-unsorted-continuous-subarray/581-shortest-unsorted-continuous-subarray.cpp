5e3e9669bed42be41fa906d61a0164cdfd1a21d4
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-08 14:51:54 +0530

    Time: 43 ms (60.05%), Space: 28.8 MB (5.80%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -10,26 +10,15 @@ public:
             v.push_back( nums[i]);
         }
         sort(v.begin(), v.end());
-        int s =0; 
-        int e= nums.size()-1;
-        int max;
-        int min;
-        
-        while(s<nums.size()){
-            if(v[s] != nums[s]){
-                min =s;
-                break;
+        int s =nums.size()-1; 
+        int e=0 ;
+        for( int i =0 ;i<nums.size();i++){
+            if(v[i] != nums[i]){
+                s = min(s,i);
+                e = max(s,i);
             }
-            s++;
         }
-        while(e>=0){
-            if(v[e] != nums[e]){
-                max =e;
-                break;
-            }
-            e--;
-        }
-        if(max-min +1<0) return 0;
-        else return max-min+1;
+        if(e-s<0) return 0;
+        else return e-s+1;
     }
 };

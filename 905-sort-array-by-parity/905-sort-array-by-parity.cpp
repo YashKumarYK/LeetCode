c2eb7e6b626d54557dbc6a0cb09296dcae930238
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-08 13:52:23 +0530

    Time: 12 ms (70.14%), Space: 16.2 MB (81.02%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        vector<int> odd;
-        vector<int> even;
-        for(int i =0; i<nums.size(); i++){
-            if (nums[i]%2 == 0){
-                even.push_back(nums[i]);
-            }
-            else{
-                odd.push_back(nums[i]);
+        // vector<int> odd;
+        // vector<int> even;
+        // for(int i =0; i<nums.size(); i++){
+        //     if (nums[i]%2 == 0){
+        //         even.push_back(nums[i]);
+        //     }
+        //     else{
+        //         odd.push_back(nums[i]);
+        //     }
+        // }
+        // sort(odd.begin(), odd.end());
+        // sort(even.begin(), even.end());
+        // for(int i=0; i<odd.size(); i++){
+        //     even.push_back(odd[i]);
+        // }
+        // return even;
+        int s=0;
+        int e= nums.size()-1;
+        while(s<=e){
+            if (nums[s]%2==0)
+                s++;
+            else if(nums[e]%2 !=0)
+                e--;
+            else if(nums[s]%2 !=0 && nums[e]%2 ==0 ){
+                swap(nums[s], nums[e]);
+                e--;
+                s++;
             }
         }
-        sort(odd.begin(), odd.end());
-        sort(even.begin(), even.end());
-        for(int i=0; i<odd.size(); i++){
-            even.push_back(odd[i]);
-        }
-        return even;
+        return nums;
         
     }
 };

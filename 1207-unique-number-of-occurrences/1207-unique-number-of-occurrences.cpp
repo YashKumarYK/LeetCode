378eb5a0e4c2f085d0533e003a78f55f39b00e44
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-14 01:19:55 +0530

    Time: 4 ms (59.13%), Space: 8.1 MB (62.53%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,10 +3,7 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         map< int , int> m;
         for( int i=0; i<arr.size(); i++){
-            if( m.find(arr[i]) !=m.end()){
-                m[arr[i]]++;
-            }
-            else m[arr[i]] = 1;
+            m[arr[i]]++;
         }
         set<int> v;
         for( auto i: m){

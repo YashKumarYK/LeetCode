b0d476ca48ec5744397c0aabf209e8be4f219bf4
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-13 13:52:32 +0530

    Time: 8 ms (57.25%), Space: 10.5 MB (65.20%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        stack<string> s;
-        for( int i=0;i<logs.size(); i++){
-            if( !s.empty() && logs[i] =="../"){
-                s.pop();
+        //approach 1 - using stack 
+        // stack<string> s;
+        // for( int i=0;i<logs.size(); i++){
+        //     if( !s.empty() && logs[i] =="../"){
+        //         s.pop();
+        //     }
+        //     else if(logs[i]=="./" || ( s.empty() && logs[i] =="../"))
+        //         continue;
+        //     else{
+        //         s.push(logs[i]);
+        //     }
+        // }
+        // return s.size();
+        
+        //approach 2 using pointer
+        int count=0;
+        for(int i =0; i<logs.size(); i++){
+            if( logs[i]== "../"){
+                count--;
+                if(count<0) count=0;
             }
-            else if(logs[i]=="./" || ( s.empty() && logs[i] =="../"))
+            else if( logs[i]=="./"){
                 continue;
+            }
             else{
-                s.push(logs[i]);
+                count++;
             }
         }
-        return s.size();
+        // if(count<=0) return 0;
+        return count;
     }
 };

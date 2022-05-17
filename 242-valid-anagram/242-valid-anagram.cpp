a569222aff4167b80d3103631742bbf940029aa9
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-17 15:56:15 +0530

    Time: 19 ms (27.25%), Space: 7.4 MB (46.87%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(t.size()!= s.size()) return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for( int i=0; i<t.size(); i++){
+            if(t[i] != s[i]) return false;
+        }
+        return true;
+    }
+};

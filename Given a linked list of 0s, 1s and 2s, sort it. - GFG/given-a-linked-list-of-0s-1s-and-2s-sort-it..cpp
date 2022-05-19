6690f9abf80357d6d2fc979c73a835c0adb4dc64
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-19 21:35:37 +0530

    Added solution - LeetHub

---
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -0,0 +1,127 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node *start = NULL;
+
+
+ // } Driver Code Ends
+/*
+ 
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    void insertAtTail( Node* &head, Node* &tail, Node* temp){
+        tail->next = temp;
+        tail = temp;
+    }
+    Node* segregate(Node *head) {
+        
+        Node* zerohead = new Node(-1);
+        Node* onehead = new Node(-1);
+        Node* twohead = new Node(-1);
+        Node* zerotail = zerohead;
+        Node* onetail = onehead;
+        Node* twotail = twohead;
+        
+        Node* temp = head;
+        while( temp!=NULL){
+            Node* newNode = new Node( temp->data);
+            if( temp->data == 0){
+                insertAtTail(zerohead, zerotail, newNode);
+                 
+            }
+            else if( temp->data == 1){
+                insertAtTail(onehead, onetail, newNode);
+            }
+            else{
+                insertAtTail(twohead, twotail, newNode);
+            }
+            temp = temp->next;
+        }
+        
+        zerotail->next =onehead->next;
+        if(onehead->next!=NULL)
+            onetail->next = twohead->next;
+        else{
+            zerotail->next = twohead->next;
+        }
+        twotail->next = NULL;
+        return zerohead->next;
+        
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Function to sort a linked list of 0s, 1s and 2s
+void printList(struct Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+void insert(int n1) {
+    int n, value, i;
+    // scanf("%d",&n);
+    n = n1;
+    struct Node *temp;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        if (i == 0) {
+            start = new Node(value);
+            temp = start;
+            continue;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+            temp->next = NULL;
+        }
+    }
+}
+
+int main() {
+
+    int n;
+
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &n);
+
+        insert(n);
+        Solution ob;
+        struct Node *newHead = ob.segregate(start);
+        printList(newHead);
+    }
+
+    return 0;
+}  // } Driver Code Ends

13b8e9bbd4ab9f9472e1e212e184d6d0c0a5c210
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-19 23:45:35 +0530

    Time: 17 ms (23.67%), Space: 11.2 MB (62.23%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -27,7 +27,7 @@ public:
         tail= tail->next;
     }
     Node* copyRandomList(Node* head) {
-        //apraoch1 - using unordered map
+        
         Node* cloneHead = NULL;
         Node* cloneTail= NULL;
         Node* temp = head;
@@ -39,18 +39,59 @@ public:
         Node* cloneNode= cloneHead;
         Node*  originalNode= head;
         
-        unordered_map<Node*, Node*> mp;
-        while( originalNode!= NULL){
-            mp[originalNode] = cloneNode;
-            cloneNode = cloneNode->next;
-            originalNode = originalNode->next;
+        //apraoch1 - using unordered map
+        // unordered_map<Node*, Node*> mp;
+        // while( originalNode!= NULL){
+        //     mp[originalNode] = cloneNode;
+        //     cloneNode = cloneNode->next;
+        //     originalNode = originalNode->next;
+        // }
+        // cloneNode=cloneHead;
+        // originalNode = head;
+        // while( originalNode!=NULL){
+        //     cloneNode->random = mp[originalNode->random];
+        //     cloneNode = cloneNode->next;
+        //     originalNode = originalNode->next;
+        // }
+        // return cloneHead;
+        
+        //approach-2 without using map:
+        //step 1- make a clone using next - done
+        //step 2 - join the clone in between the original
+        Node* next = NULL;
+        while(  originalNode!=NULL && cloneNode!=NULL){
+            //joining original node to the clone node
+            next = originalNode->next;
+            originalNode->next = cloneNode;
+            originalNode= next;
+            
+            //joining clone node to next original node
+            next = cloneNode->next;
+            cloneNode->next = originalNode;
+            cloneNode = next;
         }
-        cloneNode=cloneHead;
-        originalNode = head;
-        while( originalNode!=NULL){
-            cloneNode->random = mp[originalNode->random];
-            cloneNode = cloneNode->next;
+        
+        
+        //step 3 random ptr
+        temp = head;
+        while( temp!=NULL ){
+            if( temp->next !=  NULL){
+                temp->next->random = temp->random? temp->random->next: temp->random;
+            }
+            temp = temp->next->next;
+        }
+        
+        //STEP 4 separating the list
+        
+        originalNode= head;
+        cloneNode = cloneHead;
+        while( originalNode!=NULL &&cloneNode !=NULL){
+            originalNode->next = cloneNode->next;
             originalNode = originalNode->next;
+            
+            if( originalNode!=NULL)
+                cloneNode->next = originalNode->next;
+            cloneNode = cloneNode->next;
         }
         return cloneHead;
     }

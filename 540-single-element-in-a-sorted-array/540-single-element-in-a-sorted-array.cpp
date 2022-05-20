28f52dd93782119b01d540c8e90d73aab84fd142
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-21 03:52:49 +0530

    Time: 25 ms (86.78%), Space: 22.4 MB (10.19%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,36 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans =0;
-        for( int i=0; i< nums.size(); i++){
-            ans = ans^ nums[i];
+        //approach 1 - bit manipulation - TC -O(n)
+        // int ans =0;
+        // for( int i=0; i< nums.size(); i++){
+        //     ans = ans^ nums[i];
+        // }
+        // return ans;
+        
+        //APPROACH 2- Binary search
+        int s=0;
+        int e=nums.size()-1;
+        int mid= s+(e-s)/2;
+        
+        if (nums.size()==1) return nums[0];
+        if ( nums[0] !=nums[1]) return nums[0];
+        else if( mid ==e && nums[e] !=nums[e-1]) return nums[e];
+        
+        while( s<=e){
+            
+            if (nums[mid]!= nums[mid+1] && nums[mid] !=nums[mid-1]) return nums[mid];
+            
+            else{ 
+                if ( (mid%2 ==0 && nums[mid] == nums[mid+1] )|| (mid%2==1 && nums[mid-1]== nums[mid])){
+                    s= mid +1;
+                }
+                else
+                    e= mid-1;
+            }         
+             
+            mid = s+(e-s)/2;
         }
-        return ans;
+        return -1;
     }
 };

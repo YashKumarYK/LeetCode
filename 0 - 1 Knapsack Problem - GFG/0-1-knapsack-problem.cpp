c248eac909ee41ea2ffaccc95e04faac4c1b9960
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-25 09:33:38 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,35 +8,56 @@ class Solution
 {
     public:
    
-    int T[1002][1002] ;
-    
-    int helper( int W, int wt[], int val[], int n){
+//   //recursion solution
+//     int T[1002][1002] ;
+//     int helper( int W, int wt[], int val[], int n){
        
-       if( n ==0 || W==0) return 0;
+//       if( n ==0 || W==0) return 0;
        
-       if( T[W][n] != -1){
-           return T[W][n];
-       }
-       else if( wt[n-1] <= W){
-           T[W][n] = max( val[n-1] + helper( W-wt[n-1], wt, val, n-1), helper(W, wt, val, n-1));
-           return T[W][n];
+//       if( T[W][n] != -1){
+//           return T[W][n];
+//       }
+//       else if( wt[n-1] <= W){
+//           T[W][n] = max( val[n-1] + helper( W-wt[n-1], wt, val, n-1), helper(W, wt, val, n-1));
+//           return T[W][n];
             
            
-       }
-       else if( wt[n-1]> W){
-           T[W][n] =helper(W, wt, val, n-1);
-           return T[W][n];
-       }
-    }
-    //Function to return max value that can be put in knapsack of capacity W.
+//       }
+//       else if( wt[n-1]> W){
+//           T[W][n] =helper(W, wt, val, n-1);
+//           return T[W][n];
+//       }
+//     }
+//     //Function to return max value that can be put in knapsack of capacity W.
+//     int knapSack(int W, int wt[], int val[], int n) 
+//     { 
+       
+//       memset( T, -1, sizeof(T));
+//       return helper(W, wt, val, n);
+       
+//     }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       
-       memset( T, -1, sizeof(T));
-       return helper(W, wt, val, n);
-       //recursion solution
-       
-            
+       int T[n+1][W+1];
+       for( int i =0; i<n+1; i++){
+           for( int j=0; j<W+1; j++){
+               if ( i==0 || j==0){
+                   T[i][j]=0;
+               }
+           }
+       }
+         
+       for( int i=1; i<n+1; i++){
+           for( int j=1 ; j<W+1 ; j++){
+               if( wt[i-1]<=j){
+                   T[i][j] = max( val[i-1] + T[i-1][j-wt[i-1]] , T[i-1][j]);
+               }
+               else{
+                   T[i][j] = T[i-1][j];
+               }
+           }
+       }
+       return T[n][W];
     }
 };
 

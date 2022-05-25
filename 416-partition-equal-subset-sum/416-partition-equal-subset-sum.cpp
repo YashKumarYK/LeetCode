b38c61a2a0566c301b82f595077cd3aeec5d9f3d
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-05-25 14:21:49 +0530

    Time: 398 ms (24.61%), Space: 12 MB (59.00%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,19 +1,47 @@
 class Solution {
 public:
     // recursion with memoization
-    int solve( vector<int>nums, int n,  long long sum, vector<vector<int>> &dp){
-        if( n==0) return 0;
-        if( sum == 0) return 1;
+//     int solve( vector<int>nums, int n,  long long sum, vector<vector<int>> &dp){
+//         if( n==0) return 0;
+//         if( sum == 0) return 1;
         
-        if( dp[n][sum] != -1) return dp[n][sum]; 
+//         if( dp[n][sum] != -1) return dp[n][sum]; 
         
-        if( nums[n-1] <= sum){
-            return dp[n][sum] = solve( nums, n-1, sum-nums[n-1], dp) || solve(nums, n-1, sum, dp);
-        }
-        else {
-            return dp[n][sum] =solve( nums, n-1, sum,dp);
-        }
-    }
+//         if( nums[n-1] <= sum){
+//             return dp[n][sum] = solve( nums, n-1, sum-nums[n-1], dp) || solve(nums, n-1, sum, dp);
+//         }
+//         else {
+//             return dp[n][sum] =solve( nums, n-1, sum,dp);
+//         }
+//     }
+//     bool canPartition(vector<int>& nums) {
+        
+//         int n = nums.size();
+        
+//         //checking if the sum is even or odd
+//         //odd - never be divided into equal sum partition
+        
+//         long long  sum =0;
+        
+//         for( auto i: nums){
+//             sum += i;
+//         }
+//         if( sum %2 ==1) return false;
+        
+//         //memoizing array
+//         vector<vector<int>> dp(n+1 , vector<int> (sum+1));
+//         for(int i=0; i<n+1; i++){
+//             for(int j=0; j<sum+1; j++){
+//                 dp[i][j] = -1;
+//             }
+//         }
+//         if(solve( nums, n, sum/2, dp) == 1){
+//             return true;
+//         }
+//         return false;
+//     }
+    
+    //dynamic programming
     bool canPartition(vector<int>& nums) {
         
         int n = nums.size();
@@ -28,16 +56,27 @@ public:
         }
         if( sum %2 ==1) return false;
         
-        //memoizing array
-        vector<vector<int>> dp(n+1 , vector<int> (sum+1));
+        sum = sum/2;
+        vector<vector<bool>> dp(n+1 , vector<bool> (sum+1));
         for(int i=0; i<n+1; i++){
-            for(int j=0; j<sum+1; j++){
-                dp[i][j] = -1;
+            for( int j=0; j< sum+1; j++){
+                if( i==0) dp[i][j] = false;
+                if( j==0) dp[i][j] = true;
             }
         }
-        if(solve( nums, n, sum/2, dp) == 1){
-            return true;
+        
+        for(int i=1; i<n+1; i++){
+            for( int j=1; j< sum+1; j++){
+                
+                if( nums[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else if( nums[i-1] <= j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                }
+            }
         }
-        return false;
+        return dp[n][sum];
     }
+        
 };

c40a66f3d6ef31ec2a3148044844d286ebaa781b
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-06 03:41:04 +0530

    Time: 35 ms (49.23%), Space: 23.8 MB (99.40%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int prod=1;
+        // int prod=1;
+        // int n = nums.size();
+        // int zflag =0;
+        // for(auto i:nums){
+        //     if(i==0){
+        //         zflag++;
+        //     }
+        //     if( i !=0)
+        //         prod *= i;
+        // }
+        // vector<int> ans (n, prod);
+        // for(int i=0; i<n; i++){
+        //     if( nums[i]!=0)
+        //         ans[i] = ans[i]/nums[i];
+        //     if( zflag!=0){
+        //         ans[i]=0;
+        //     }
+        //     if( zflag ==1 && nums[i]==0){
+        //         ans[i]= prod;
+        //     }
+        // }
+        // return ans;
         int n = nums.size();
-        int zflag =0;
-        for(auto i:nums){
-            if(i==0){
-                zflag++;
-            }
-            if( i !=0)
-                prod *= i;
+        vector<int> v(n);
+        int prod=1;
+        for(int i=0; i<n;i++){
+            v[i]= prod;
+            prod*= nums[i];
         }
-        vector<int> ans (n, prod);
-        for(int i=0; i<n; i++){
-            if( nums[i]!=0)
-                ans[i] = ans[i]/nums[i];
-            if( zflag!=0){
-                ans[i]=0;
-            }
-            if( zflag ==1 && nums[i]==0){
-                ans[i]= prod;
-            }
+        prod =1;
+        for( int i=n-1; i>=0; i--){
+            int temp = nums[i];
+            nums[i]= prod;
+            prod*= temp;
         }
-        return ans;
+        
+        for( int i=0; i<n; i++){
+            v[i]= v[i]* nums[i];
+        }
+        return v;
     }
 };

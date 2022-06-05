0e0d584103b337376e6ab09f096a4f6947a140bc
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-05 21:30:39 +0530

    Time: 415 ms (40.05%), Space: 6.5 MB (83.87%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -10,27 +10,43 @@ public:
         }
         return true;
     }
-    int solve( int index, int n, string &s, vector<int> &dp){
-        //base case
-        if( index ==n) return 0;
+   // memoization
+//     int solve( int index, int n, string &s, vector<int> &dp){
+//         //base case
+//         if( index ==n) return 0;
         
-        if( dp[index] != -1) return dp[index];
+//         if( dp[index] != -1) return dp[index];
         
-        //main part
-        int mini = INT_MAX;
+//         //main part
+//         int mini = INT_MAX;
         
-        for(int j =index ; j<n; j++){
-            if( ispalindrome( s, index, j)){
-                int cost = 1+ solve( j+1, n, s, dp);
-                mini = min(mini, cost);
-            }
-        }
-        return dp[index] =mini;
-    }
+//         for(int j =index ; j<n; j++){
+//             if( ispalindrome( s, index, j)){
+//                 int cost = 1+ solve( j+1, n, s, dp);
+//                 mini = min(mini, cost);
+//             }
+//         }
+//         return dp[index] =mini;
+//     }
     int minCut(string s) {
         int n = s.length();
-        vector<int> dp(n,-1);
+//         vector<int> dp(n,-1);
+//         return solve(0, n, s, dp)-1;
         
-        return solve(0, n, s, dp)-1;   
+        //Dynamic Programming
+        vector<int> dp(n+1, 0);
+        int mini = INT_MAX;
+        for(int i= n-1; i>=0; i--){
+            int mini = INT_MAX;
+        
+            for(int j =i ; j<n; j++){
+                if( ispalindrome( s, i, j)){
+                    int cost = 1+ dp[j+1];
+                    mini = min(mini, cost);
+                }
+            }
+            dp[i] =mini;
+        }
+        return dp[0] -1;
     }
 };

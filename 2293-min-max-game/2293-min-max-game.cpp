b0285cd21356db2e0c15753089304658722219f8
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-06 01:51:17 +0530

    Time: 3 ms (100.00%), Space: 9.9 MB (58.33%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -6,12 +6,13 @@ public:
             vector<int> newNums(n/2);
             for(int i=0; i<n/2; i++){
                 if( i%2 ==0){
-                    nums[i]= min( nums[2*i], nums[2*i +1]);
+                    newNums[i]= min( nums[2*i], nums[2*i +1]);
                 }
                 else{
-                    nums[i] = max( nums[2*i], nums[2*i+1]);
+                    newNums[i] = max( nums[2*i], nums[2*i+1]);
                 }
             }
+            nums = newNums;
             n=n/2;
         }
         return nums[0];

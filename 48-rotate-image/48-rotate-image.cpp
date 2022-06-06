793694a08815f492561df03866d6a1245a049201
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-06 16:37:55 +0530

    Time: 3 ms (58.99%), Space: 7.2 MB (34.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,18 +3,30 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c= matrix[0].size();
-        vector<int> v;
-        for(int j = 0 ; j<c ; j++){
-            for( int i= r-1; i>=0; i--){
-                v.push_back(matrix[i][j]);
+//         vector<int> v;
+//         for(int j = 0 ; j<c ; j++){
+//             for( int i= r-1; i>=0; i--){
+//                 v.push_back(matrix[i][j]);
+//             }
+//         }
+//         int k =0;
+        
+//         for( int i =0; i< r; i++){
+//             for( int j=0; j<c; j++){
+//                 matrix[i][j] = v[k++];
+//             }
+//         }
+        
+        //approach #2 
+        // inplace 
+        for(int i=0; i<r; ++i){
+            for( int j=i+1; j<c; ++j){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        int k =0;
         
-        for( int i =0; i< r; i++){
-            for( int j=0; j<c; j++){
-                matrix[i][j] = v[k++];
-            }
+        for(int i =0; i<r; ++i){
+            reverse( matrix[i].begin(),matrix[i].end());
         }
         
     }

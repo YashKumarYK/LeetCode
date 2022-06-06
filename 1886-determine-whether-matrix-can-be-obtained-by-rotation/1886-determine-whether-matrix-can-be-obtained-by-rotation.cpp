66d9ff55afb384dfab10afe6170e54a9cec07ce5
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-06 16:57:31 +0530

    Time: 12 ms (15.60%), Space: 11 MB (91.52%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -8,9 +8,8 @@ public:
         }
         while(cnt--){
             for( int i=0; i<n; i++){
-                for( int j =i; j<n; j++){
-                    if( i!=j)
-                        swap(mat[i][j], mat[j][i]);
+                for( int j =i+1; j<n; j++){
+                    swap(mat[i][j], mat[j][i]);
                 }
             }
             for( int i=0; i<n; i++){

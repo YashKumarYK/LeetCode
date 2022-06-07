328af80fb99fb59d48d90403abb29d4c872554e0
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-07 16:03:41 +0530

    Time: 29 ms (35.25%), Space: 8.6 MB (33.93%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    int solve( vector<int> &arr, int i, int k, int n, vector<int> &dp){
-        if( i==n) return 0;
-        if( dp[i] !=-1) return dp[i];
-        int maxi = INT_MIN;
-        int maxAns = INT_MIN;
-        int l =0;
-        for( int j=i ; j < min(n,k+i); j++){
-            l++;
-            maxi = max( maxi, arr[j]);
-            int sum = maxi* l + solve(arr, j+1, k,n, dp);
-            maxAns = max( sum, maxAns);
-        }
-        return dp[i] = maxAns;
-    }
+    //recursion + memoization
+    // int solve( vector<int> &arr, int i, int k, int n, vector<int> &dp){
+    //     if( i==n) return 0;
+    //     if( dp[i] !=-1) return dp[i];
+    //     int maxi = INT_MIN;
+    //     int maxAns = INT_MIN;
+    //     int l =0;
+    //     for( int j=i ; j < min(n,k+i); j++){
+    //         l++;
+    //         maxi = max( maxi, arr[j]);
+    //         int sum = maxi* l + solve(arr, j+1, k,n, dp);
+    //         maxAns = max( sum, maxAns);
+    //     }
+    //     return dp[i] = maxAns;
+    // }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n,-1);
-        return solve( arr, 0, k,n, dp);
+        // vector<int> dp(n,-1);
+        // return solve( arr, 0, k,n, dp);
+        
+        
+        //top down approach- tabulation
+        vector<int> dp(n+1);
+        dp[n]=0;
+        for( int i=n-1; i>=0; i--){
+            int maxi = INT_MIN;
+            int maxAns = INT_MIN;
+            int l =0;
+            for( int j=i ; j < min(n,k+i); j++){
+                l++;
+                maxi = max( maxi, arr[j]);
+                int sum = maxi* l + dp[j+1];
+                maxAns = max( sum, maxAns);
+            }
+            dp[i] = maxAns;
+        }
+        return dp[0];
     }
 };

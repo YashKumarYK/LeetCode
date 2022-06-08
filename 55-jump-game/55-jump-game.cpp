2f7b6a2a892496b0c23ae8b3a3018a56506f5cc1
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-08 20:59:34 +0530

    Time: 89 ms (56.54%), Space: 48.2 MB (99.69%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -19,16 +19,26 @@ public:
         if(nums.size()==1)
             return true;
         // return solve( nums, 0, n, dp);
-        vector<bool> dp(n+1);
-        dp[0]=true;
-        for(int i=1; i<n; i++){
-            for( int j = i-1 ; j>=0; j--){
-                    if(dp[j] && j+nums[j]>=i){
-                        dp[i]= true;
-                        break;
-                    }
-            }
+        
+        //DP approach
+        // vector<bool> dp(n+1);
+        // dp[0]=true;
+        // for(int i=1; i<n; i++){
+        //     for( int j = i-1 ; j>=0; j--){
+        //             if(dp[j] && j+nums[j]>=i){
+        //                 dp[i]= true;
+        //                 break;
+        //             }
+        //     }
+        // }
+        // return dp[n-1];
+        
+        ///greedy
+        int reach =0;
+        for( int i=0; i<n; i++){
+            if (reach<i) return false;
+            reach = max(reach, i+nums[i]);
         }
-        return dp[n-1];
+        return true;
     }
 };

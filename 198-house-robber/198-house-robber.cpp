239fc08f47bdf98ddd2dbbbdff59d08b1ac2298d
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-08 18:16:12 +0530

    Time: 3 ms (50.38%), Space: 7.8 MB (47.56%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    //memoization
-    int solve( vector<int> nums , int n, int index, int dp[]){
-        if( index >= n) return 0;
-        
+    int solve( vector<int> &nums, int n, int index, vector<int> &dp){
+        if(index >= n){
+            return 0;
+        }    
         if( dp[index] != -1) return dp[index];
-        
-        int a= solve( nums, n, index+2, dp) + nums[index];
-        int b = solve( nums, n , index+1, dp);
-        
-        dp[index] = max(a, b);
-        return dp[index];
+        int maxi = INT_MIN;
+        for( int i=index; i<n; i++){
+            int cost= INT_MIN;
+            // if( i<n-2)
+                cost = nums[i] + solve(nums, n, i+2, dp);
+            maxi = max( cost, maxi);
+        }
+        return dp[index] = maxi;
     }
+    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int index =0;
-        int dp[n+1];
-        memset(dp,-1, sizeof(dp));
-        
+        int index=0;
+        vector<int> dp(n+1, -1);
         return solve( nums, n, index, dp);
-        
     }
 };

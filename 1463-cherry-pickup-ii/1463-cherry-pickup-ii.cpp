6c6d714f389d649c983c7858df135218b8f37758
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-06-11 17:19:15 +0530

    Time: 105 ms (54.10%), Space: 14.4 MB (20.60%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solve( int i, int j1, int j2,int r, int c, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp){
+        //base condition
+        //out of bound condition
+        if( j1<0||j1>=c||j2<0||j2>=c) return -1e8;
+        
+        //destination condition
+        if( i== r-1){
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+ grid[i][j2];
+        }
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        
+        int maxi =-1e8;
+        //9 paths 
+        for( int dj1 = -1 ; dj1<=1; dj1++){
+            for(int dj2 =-1; dj2<=1; dj2++){
+                int value =0;
+                if( j1==j2) value =grid[i][j1];
+                    
+                else value =grid[i][j1]+ grid[i][j2];
+                value += solve( i+1, j1+dj1, j2+dj2, r,c, grid, dp);
+                maxi= max(value, maxi);
+            
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+        vector<vector<vector<int>>> dp(r, vector<vector<int>> (c, vector<int> (c,-1)));
+        return solve( 0,0,c-1,r,c, grid, dp);
+    }
+};

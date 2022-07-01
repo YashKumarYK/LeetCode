c9fbf62e4c772822d6d41cd303ab85227b6dd607
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-01 09:39:52 +0530

    Time: 20 ms (47.93%), Space: 7.7 MB (68.35%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    
-    void solve( set<long long> &s, long long num){
-        if(num >=INT_MAX) return;
-        if( s.find(num)!=s.end()) return;
-        s.insert(num);
-        solve(s, num*2);
-        solve(s, num*3);
-        solve(s, num*5);
-    }
     int nthUglyNumber(int n) {
-        set<long long> s;
-        solve( s, 1);
-        int ans =n;
-        for( auto i: s){
-            n--;
-            if( n==0) ans = i;
+        vector<int> ans(n, 0);
+        ans[0] =1;
+        int p2=0;
+        int p3=0;
+        int p5=0;
+       for(int i=1;i<n;++i){
+            int el = min({ans[p2]*2, ans[p3]*3, ans[p5]*5});
+            ans[i] =el;
+            if( ans[p2]*2==el) p2++;
+            if( ans[p3]*3==el) p3++;
+            if( ans[p5]*5==el) p5++;
         }
-        return ans;
+        return ans[n-1];
     }
 };

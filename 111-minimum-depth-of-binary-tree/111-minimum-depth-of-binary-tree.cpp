50bcce8c2e4f981f9c23f89ddabf013317114321
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-04 03:50:05 +0530

    Time: 483 ms (14.97%), Space: 144.7 MB (43.63%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -11,25 +11,20 @@
  */
 class Solution {
 public:
-    
-    int travel( TreeNode* root){
+    int minDepth(TreeNode* root) {
         if( root == NULL){
             return 0;
         }
         if( root->left==NULL && root->right !=NULL){
-            return 1+ travel(root->right);
+            return 1+ minDepth(root->right);
         }
         if( root->right ==NULL && root->left !=NULL){
-            return 1+ travel(root->left);
+            return 1+ minDepth(root->left);
         }
         
-        int left = travel( root->left);
-        int right = travel( root->right);
+        int left = minDepth( root->left);
+        int right = minDepth( root->right);
         
         return min( left, right)+1;
-    }
-    int minDepth(TreeNode* root) {
-        if( root== NULL ) return 0;
-        return travel( root);
     }
 };

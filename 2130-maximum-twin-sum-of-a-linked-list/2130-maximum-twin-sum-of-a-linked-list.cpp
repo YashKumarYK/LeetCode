855ef61bec3542eef60bdf882656bad9b48aafe4
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-06 17:33:13 +0530

    Time: 587 ms (5.07%), Space: 116.5 MB (98.78%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,20 +10,57 @@
  */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
-        vector<int> v;
-        ListNode* temp = head;
-        while( temp!=NULL){
-            v.emplace_back(temp->val);
-            temp = temp->next;
+    
+    ListNode* reverse( ListNode* head){
+        ListNode* next = NULL;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        
+        while( curr!= NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        int n = v.size();
-        int i=0;
-        int j = n-1;
+        return prev;
+    }
+    int pairSum(ListNode* head) {
+        // vector<int> v;
+        // ListNode* temp = head;
+        // while( temp!=NULL){
+        //     v.emplace_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // int n = v.size();
+        // int i=0;
+        // int j = n-1;
+        // int maxi = INT_MIN;
+        // while( i<j){
+        //     maxi = max(maxi, v[i]+ v[j]);
+        //     i++, j--;
+        // }
+        // return maxi;
+        
+        //second approach
+        ListNode* fast= head;
+        ListNode* slow = head;
+        while( fast->next!= NULL && fast->next->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        cout<<slow->val<<endl;
+        
+        ListNode* mid = reverse( slow->next);
+        ListNode* temp = head;
+        
         int maxi = INT_MIN;
-        while( i<j){
-            maxi = max(maxi, v[i]+ v[j]);
-            i++, j--;
+        // mid= mid->next;
+        
+        while( mid!= NULL){
+            maxi = max(maxi, temp->val + mid->val);
+            temp= temp->next;
+            mid = mid->next;
         }
         return maxi;
     }

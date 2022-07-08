6d4aaa055f87504610f092758c5efed793997383
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-08 20:27:51 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (5.80%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    int integerBreak(int n) {
-        int ans =1;
-        if( n<4) return n-1;
-        while( n>4){
-            ans*= 3;
-            n-=3;
+    
+    int solve( int i , vector<int> &num, int sum, vector<vector<int>> &dp){
+        if( i==num.size() || sum==0){
+            return 1;
         }
-        return n*ans;
+        if( dp[i][sum] !=-1) return dp[i][sum];
+        if( num[i]<=sum){
+            return dp[i][sum] = max( num[i]* solve( i, num, sum-num[i], dp), solve( i+1, num, sum, dp));
+        }
+        else{
+            return dp[i][sum] = solve(i+1, num, sum, dp);
+        }
+    }
+    int integerBreak(int n) {
+        vector<int> num(n-1);
+        for(int i=0; i<n-1; i++){
+            num[i]= i+1;
+        }
+        vector<vector<int>> dp( n-1,vector<int> (n+1,-1));
+        return solve(0, num, n, dp);
     }
 };

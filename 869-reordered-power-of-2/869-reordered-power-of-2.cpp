f0a55ed9777fb174b9a6c2e65e4cd2fb3a91e728
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-21 19:52:51 +0530

    Time: 1370 ms (5.20%), Space: 5.9 MB (95.84%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool power2( string &v){
+        if( v[0]=='0' ) return false;
+        int n = stoi(v);
+        return (n&(n-1))==0;
+    }
+    
+    bool solve( string v, int ind){
+        if( ind == v.size()){
+            return power2(v);
+        }
+        
+        
+        for(int i = ind ; i<v.size(); i++){
+            swap(v[i], v[ind]);
+            
+            if( solve( v, ind+1)) return true;
+            
+            swap(v[i], v[ind]);
+        }
+        return false;
+    }
+    
+    bool reorderedPowerOf2(int n) {
+        vector<int> v;
+        string s = to_string(n);
+  
+        return solve( s, 0);
+    }
+};

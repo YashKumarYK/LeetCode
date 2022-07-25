a29832515250afb942aeddd73cc9884a5dbb275f
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-26 02:23:10 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (11.96%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int dp[20];
-    int solve( int n ){
-        if( n==0) return 1;
-        if( n==1) return 1;
-        if( n==2) return 2;
-        if( dp[n]!= -1) return dp[n];
-        int count=0;
-        for(int k=1; k<=n; k++){
-            count+= solve(k-1)* solve(n-k);
-        }
-        return dp[n] = count;
+//     int dp[20];
+//     int solve( int n ){
+//         if( n==0) return 1;
+//         if( n==1) return 1;
+//         if( n==2) return 2;
+//         if( dp[n]!= -1) return dp[n];
+//         int count=0;
+//         for(int k=1; k<=n; k++){
+//             count+= solve(k-1)* solve(n-k);
+//         }
+//         return dp[n] = count;
         
+//     }
+    int tab( int n){
+        vector<int> dp(n+1, 0);
+        dp[0]=dp[1]=1;
+        for(int i=2; i<=n; i++){
+            for(int k=1; k<=i; k++){
+                dp[i] += dp[k-1]* dp[i-k];
+            }
+        }
+        return dp[n];
     }
     int numTrees(int n) {
         //catalans number
-        memset(dp, -1, sizeof(dp));
-        return solve( n);
+        // memset(dp, -1, sizeof(dp));
+        return tab( n);
     }
 };

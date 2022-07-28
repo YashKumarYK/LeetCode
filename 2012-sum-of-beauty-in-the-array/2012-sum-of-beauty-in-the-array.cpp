40fbff12e705facad5841ccf04949886d6ec0ee4
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-29 01:13:33 +0530

    Time: 262 ms (40.67%), Space: 88.5 MB (55.13%) - LeetHub

--- 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int sumOfBeauties(vector<int>& nums) {
-        bool first = true;
-        int score=0;
-        vector<vector<int>> v(nums.size(), vector<int> (2,0));
+
+        vector<int> v1( nums.size());
+        vector<int> v2( nums.size());
+        
         int mini = INT_MAX;
         int maxi = INT_MIN;
+        int n = nums.size();
         for(int i=0; i<nums.size(); i++){
-            v[i][0]= maxi;
+            v1[i]= maxi;
             if( maxi < nums[i]){
                 maxi = nums[i];
             }
-            
-        }
-        for( int i= nums.size()-1; i>=0; i--){
-            v[i][1] = mini;
-            if( mini > nums[i]){
-                mini = nums[i];
+            v2[n-i-1]= mini;
+            if( mini > nums[n-i-1]){
+                mini = nums[n-i-1];
             }
-            
         }
         int count =0;
         for( int i=1; i<nums.size()-1; i++){
-            if( nums[i]>v[i][0] && nums[i]<v[i][1]){
+            if( nums[i]>v1[i] && nums[i]<v2[i]){
                 count+=2;
             }
             else if(nums[i]>nums[i-1]&& nums[i]<nums[i+1]){

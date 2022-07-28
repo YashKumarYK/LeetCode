3b33fc5c368ff73dd0ca0063db03b45af0b70af1
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-07-28 12:12:58 +0530

    Time: 11 ms (30.55%), Space: 10.1 MB (20.51%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        vector<int> v(prices.size());
         stack<int> s;
         s.push(0);
-        vector<int> ans( prices.size(),0);
-        for( int i=prices.size()-1; i>=0; --i){
-            while( s.top()!=0 && s.top()>prices[i]){
-                s.pop();
+        for(int i = prices.size()-1; i >=0 ; i --){
+            if(s.top()==0) {
+                v[i] = prices[i];
             }
-            ans[i]= prices[i]-s.top();
+            else if(s.top()!=0 && s.top()<=prices[i]){
+                v[i] =prices[i] - s.top();
+            }
+            else if(s.top()!=0 && s.top () >prices[i]){
+                while(s.top()!=0 && s.top () >prices[i]){
+                    s.pop();
+                }
+                if(s.top()==0){
+                    v[i] = prices[i];
+                }
+                if(s.top()<=prices[i]){
+                    v[i] = prices[i] -s.top();
+                }
+            }
             s.push(prices[i]);
         }
-        return ans;
+        return v;
     }
 };

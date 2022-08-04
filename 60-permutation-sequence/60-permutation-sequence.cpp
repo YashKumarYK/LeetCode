c76d4bf63bf2a20cc107c49d8b77b40266468068
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-08-04 07:59:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (51.15%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     
-    void solve( int fact, vector<int> & v, int k, string &s){
-        if( v.size()==0){
-            return ;    
-        }
-        
-        s = s+ to_string(v[k/fact]);
-        v.erase(v.begin() + k/fact);
-        if( v.size()!=0)
-            solve( fact/v.size(), v, k%fact, s);
-    }
     string getPermutation(int n, int k) {
         //calculating the factorial of n-1 numbers and making and array of numbers 1->n
         int fact =1;
@@ -21,7 +11,15 @@ public:
         }v.push_back(n);
         k= k-1; //for 0-based indexing
         string s ="";
-        solve( fact, v, k, s);
+        while( true){
+            s += to_string( v[ k/fact]);
+            v.erase( v.begin()+ k/fact);
+             k= k%fact;
+            if( v.size()==0) break;
+            fact /=v.size();
+            
+        }
+       
         return s;
     }
 };

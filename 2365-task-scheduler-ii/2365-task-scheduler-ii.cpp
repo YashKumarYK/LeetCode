788fbeaa3f865a21a4c3d4874c113acd3be60177
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-08-18 04:19:04 +0530

    Time: 300 ms (89.94%), Space: 102.1 MB (23.57%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -7,17 +7,10 @@ public:
         mp[tasks[0]]= 1;
         for( int i=1; i<tasks.size(); i++){
             day = day +1;
-            if( mp.find(tasks[i])==mp.end()){
-                mp[tasks[i]]= day;
-                
-            }
-            else {
-                if(day-mp[tasks[i]]<=space)
-                    day= mp[tasks[i]]+ space+1;
-                mp[tasks[i]] = day;
-            }
+            if(mp.find(tasks[i])!= mp.end() && day-mp[tasks[i]]<=space)
+                day= mp[tasks[i]]+ space+1;
+            mp[tasks[i]] = day;
         }
-
         return day;
     }
 };

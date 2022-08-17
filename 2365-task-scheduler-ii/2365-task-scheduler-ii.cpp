3e85b1c8c1fbed94d4be041d01f016853114e165
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-08-18 04:10:47 +0530

    Time: 356 ms (75.05%), Space: 102.1 MB (49.62%) - LeetHub

--- 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     long long taskSchedulerII(vector<int>& tasks, int space) {
         unordered_map<long long,long long> mp;
-        vector<long long> v(tasks.size());
-        v[0]=1;
+        // vector<long long> v(tasks.size());
+        long long day=1;
         mp[tasks[0]]= 1;
-        // cout<<v[0]<<" ";
         for( int i=1; i<tasks.size(); i++){
-            v[i] = v[i-1] +1;
+            day = day +1;
             if( mp.find(tasks[i])==mp.end()){
-                mp[tasks[i]]= v[i];
+                mp[tasks[i]]= day;
                 
             }
             else {
-                if(v[i]-mp[tasks[i]]<=space)
-                    v[i]= mp[tasks[i]]+ space+1;
-                mp[tasks[i]] = v[i];
+                if(day-mp[tasks[i]]<=space)
+                    day= mp[tasks[i]]+ space+1;
+                mp[tasks[i]] = day;
             }
-            // cout<<tasks[i] << "->" << mp[tasks[i]]<<" "<<endl;
         }
 
-        return v[v.size()-1];
+        return day;
     }
 };

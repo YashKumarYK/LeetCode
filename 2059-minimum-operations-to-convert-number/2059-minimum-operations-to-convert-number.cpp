522bb3fd5518a86d1726332cf41640898f4b07f1
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-08-26 21:33:57 +0530

    Time: 973 ms (32.34%), Space: 134.4 MB (37.26%) - LeetHub

--- 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.cpp
+++ 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
+    
+    
+    int op(int x, int num, int a){
+        if( a== 0){
+            x-=num;
+        }
+        else if( a==1){
+            x+=num;
+        }
+        else{
+            x^=num;
+        }
+        return x;
+    }
     int minimumOperations(vector<int>& nums, int start, int goal) {
         int n = nums.size();
         queue<int> q;
-        unordered_map<int, int> s;
+        set<int> s;
         
         q.push(start);
         int count =0;
@@ -17,13 +31,13 @@ public:
                 if( front<0 || front >1000 || s.count(front)){
                     continue;
                 }
-                // s.insert(front);
-                s[front]++;
+                s.insert(front);
                 
                 for( int i=0; i<n; ++i){
-                    q.push( front+ nums[i]);
-                    q.push( front-nums[i]);
-                    q.push( front^nums[i]);
+                    for(int j=0; j<3; j++){
+                        int x = op(front, nums[i], j);
+                        q.push(x);
+                    }
                 }
             }
             count++;

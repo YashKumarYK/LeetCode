e632fb84438ca9a413d7bfe64e623bb37d5b7efa
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-09-13 12:00:07 +0530

    Time: 255 ms (10.23%), Space: 32.4 MB (12.47%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& in) {
         int n = in.size();
-        vector<int> ans;
-        unordered_map<int,int> mp;
-        vector<int> v1;
+        vector<int> ans(n);
+        map<int,int> mp;
         int ind=0;
         for( auto i: in){
-            v1.push_back( i[0]);
             mp[i[0]]=ind++;
         }
-        sort( v1.begin(), v1.end());
         
+        ind =0;
         for( auto i: in){
-            
-            if(i[1]> v1[n-1]) ans.push_back(-1);
-            else{
-                int ind = lower_bound( v1.begin(), v1.end(), i[1])- v1.begin();
-                ans.push_back(mp[v1[ind]]);
-            }    
+            ans[ind++]=  mp.lower_bound(i[1])!= mp.end()? mp.lower_bound(i[1])->second: -1;
         }
         
         return ans;

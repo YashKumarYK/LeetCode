29623d84701eb9ee85cdc473415ba989c73fef4a
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-09-30 00:06:13 +0530

    Time: 101 ms (55.53%), Space: 31.9 MB (61.64%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -3,49 +3,61 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int ind = lower_bound( arr.begin(), arr.end(), x)- arr.begin();
         cout<<ind<<" ";
-        priority_queue<int> maxh;
-        priority_queue<int, vector<int>, greater<int>> minh;
+//         priority_queue<int> maxh;
+//         priority_queue<int, vector<int>, greater<int>> minh;
         vector<int> ans;
         int n = arr.size();
-        for( int i=ind-1; i>=0; i--){
-            maxh.push(arr[i]);
-        }
-        for( int i= ind; i<n; ++i){
-            minh.push(arr[i]);
-        }
+//         for( int i=ind-1; i>=0; i--){
+//             maxh.push(arr[i]);
+//         }
+//         for( int i= ind; i<n; ++i){
+//             minh.push(arr[i]);
+//         }
         
-        while( k>0 && !maxh.empty() && !minh.empty()){
-            int t1 = maxh.top();
-            int t2 = minh.top();
-            if( abs(t1-x)> abs(t2-x)){
-                ans.push_back(t2);
-                minh.pop();
+//         while( k>0 && !maxh.empty() && !minh.empty()){
+//             int t1 = maxh.top();
+//             int t2 = minh.top();
+//             if( abs(t1-x)> abs(t2-x)){
+//                 ans.push_back(t2);
+//                 minh.pop();
+//             }
+//             else if(abs(t1-x) < abs(t2-x)){
+//                 ans.push_back(t1);
+//                 maxh.pop();
+//             }
+//             else{
+//                 ans.push_back( t2>t1?t1:t2);
+//                 t1<t2? maxh.pop():minh.pop();
+//             }
+//             k--;
+//         }
+        int i= ind-1, j = ind;
+        while( i>=0 && j<n && k>0){
+            int a = arr[i], b = arr[j];
+            if( abs(a-x)< abs(b-x)){
+                ans.push_back(a);
+                i--;
             }
-            else if(abs(t1-x) < abs(t2-x)){
-                ans.push_back(t1);
-                maxh.pop();
+            else if( abs( a-x)> abs(b-x)){
+                ans.push_back(b);
+                j++;
             }
             else{
-                ans.push_back( t2>t1?t1:t2);
-                t1<t2? maxh.pop():minh.pop();
+                ans.push_back(a>b?b:a);
+                if(a>b) j++;
+                else i--;
             }
             k--;
         }
-        if(k>0){
-            if( maxh.empty()){
-                while( k>0 && !minh.empty()){
-                    k--;
-                    ans.push_back(minh.top());
-                    minh.pop();
-                }
-            }
-            else if( minh.empty()){
-                while(k>0 && !maxh.empty()){
-                    k--;
-                    ans.push_back(maxh.top());
-                    maxh.pop();
-                }
-            }
+        while(k>0 && j<n){
+            ans.push_back(arr[j]);
+            j++;
+            k--;
+        }
+        while(k>0 && i>=0){
+            ans.push_back(arr[i]);
+            i--;
+            k--;
         }
         sort(ans.begin(), ans.end());
         return ans;

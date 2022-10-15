61408bf405c3b56af9c2b0e743e5284c8052a443
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2022-10-16 01:11:33 +0530

    Time: 471 ms (13.50%), Space: 60.5 MB (72.59%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
@@ -1,31 +1,6 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-//         int n1=0;
-//         int n2=0;
-//         if( nums2.size()%2==0 && nums1.size()%2==0) return 0;
-//         if( nums1.size()%2!=0){
-//             unordered_map<int, int> mp;
-//             for( int i=0; i<nums2.size(); ++i){
-//                 mp[nums2[i]]++;
-//             }
-//             for( auto i:mp){
-//                 if(i.second%2!=0) n2 = n2^i.first;  
-//             }
-//             return n2;
-//         }
-//         if( nums2.size()%2!=0){
-//             unordered_map<int, int> mp;
-//             for( int i=0; i<nums1.size(); ++i){
-//                 mp[nums1[i]]++;
-//             }
-           
-//             for( auto i:mp){
-//                 if(i.second%2!=0) n1 = n1^i.first;  
-//             }
-//         }
-//         return n1^n2;
-       // / {
         int n1=0;
         int n2=0;
         for( int i=0; nums2.size()%2!=0 && i<nums1.size(); ++i){

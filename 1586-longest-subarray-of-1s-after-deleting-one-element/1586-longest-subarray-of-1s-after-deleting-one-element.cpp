81582c3d659225a77130f0ab958cd2d5b306daf0
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-07-05 18:38:42 +0530

    Time: 34 ms (95.98%), Space: 36.5 MB (65.98%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        vector<pair<int,int>> p;
         int n = nums.size();
+        int maxi = 0;
+        int prevlen = -1;
+        int prevend = 0;
         for(int i=0;i<n;i++){
             if(nums[i]==1){
                 int start = i;
                 while(i<n-1 && nums[i+1]==1){
                     i++;
                 }
-                // cout<<i-start+1<<endl;
-                p.push_back({start, i-start+1});
-            }
-        }
-        if(p.size()==0) return 0;
-        
-        int maxi =p[0].second;
-        if(maxi== n) return maxi-1;
-        for(int i=1;i<p.size(); i++){
-            if(p[i-1].first+p[i-1].second+1== p[i].first){
-                maxi = max(maxi, p[i-1].second + p[i].second);
-            }
-            maxi = max({maxi, p[i-1].second, p[i].second});
+                int currlen = i-start+1;
+                if(prevend!= -1 && prevend+2 == start){
+                    maxi = max(maxi, prevlen+currlen);
+                }
+                maxi = max(currlen, maxi);
 
+                prevend = i;
+                prevlen = currlen;
+            }
         }
+        if(maxi == n) maxi--;
         return maxi;
-    
     }
 };

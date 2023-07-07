290d1da70ab129f3551cdc2d067738cf8a4c3158
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-07-08 01:44:18 +0530

    Time: 23 ms (88.48%), Space: 10 MB (58.72%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int slidingWindow(string &a, int k, char ch){
-        int i=0;
-        int j=0;
-        int n = a.size();
-        int cnt =0;
+    int maxConsecutiveAnswers(string a, int k) {
+        int i=0, j=0, n= a.size();
         int len = 0;
+        int Tcnt = 0, Fcnt =0;
         while(j<n){
-            if(a[j]==ch) cnt++;
-            if(cnt>k){
-                while(cnt>k){
-                    if(a[i]==ch){
-                        cnt--;
-                    }
-                    i++;
-                }
+            if(a[j]=='T') Tcnt++;
+            else Fcnt++;
+            while( min(Fcnt, Tcnt)>k){
+                if(a[i]=='T') Tcnt--;
+                else Fcnt--;
+                i++;
             }
-            if(cnt<=k){
-                len = max(len, j-i+1);
-            }
+            len = max(len, Fcnt+ Tcnt);
             j++;
         }
         return len;
-    }
-    int maxConsecutiveAnswers(string a, int k) {
-        int ans1 = slidingWindow(a, k, 'T');
-        int ans2 = slidingWindow(a, k, 'F');
-        return max(ans1, ans2);
     }
 };

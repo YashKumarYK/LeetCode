7e5bbf3d821264c9deb0c5c9c78e0bd6c7f22b14
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-08-17 03:19:32 +0530

    Time: 3 ms (84.17%), Space: 13.9 MB (64.22%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,38 +2,32 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
-        
-        /*have to check case -
-        1. if arr[num]==arr[left]==arr[right]
-        2. whether we are in first half 
-            -> if we are in first half, if are going to check whether the target lies in first half on not
-            ->if we have to do the same for the second half */
-        
-        int left =0;
-        int right =n-1;
-        
-        while( left<=right){
-            int mid = left+(right-left)/2;
-            if( nums[mid]== target) return true;
-            
-            if( nums[mid]== nums[left] && nums[mid]== nums[right]) {
-                left++;
-                right--;
+        int s = 0;
+        int e = nums.size()-1;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[m]==target) return true;
+            if(nums[s]==nums[m] && nums[m]==nums[e]){ //for cases like [1,1,1,1,2,1,1] target = 2;
+                s++;
+                e--;
             }
-            
-            //checking the halves
-            else if( nums[left]<= nums[mid]){
-                //we are in first half right now
-                if( target>= nums[left] && target < nums[mid]) right = mid-1;
-                else left = mid+1;
+            else if(nums[s]<=nums[m]){  // left part is sorted
+                //check if target exists or not
+                if(nums[s]<= target && target<= nums[m]){
+                    e = m-1;
+                }else{
+                    s = m+1;
+                }
             }
-            else{
-                if( target> nums[mid] && target<= nums[right]) left = mid+1;
-                else right = mid-1;
+            else{// right part is sorted
+                //check if target exists or not
+                if(nums[m]<= target && target<= nums[e]){
+                    s = m+1;
+                }else{
+                    e = m-1;
+                }
             }
-           
         }
-        
         return false;
     }
 };

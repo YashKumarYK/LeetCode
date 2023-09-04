34c5b2ae070560429f597986c00fcbb8ad8be182
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-09-04 17:42:37 +0530

    Time: 51 ms (90.91%), Space: 16.5 MB (27.27%) - LeetHub

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/2978-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/2978-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -3,17 +3,28 @@ public:
     bool checkStrings(string s1, string s2) {
         if(s1==s2) return true;
         int n = s2.size();
-        for(int i=0;i<n;i++){
-            if(s1[i]==s2[i]) continue;
-            bool possible = false;
-            for(int j = i; j<n; j= j+2){
-                if(s1[i]==s2[j]){
-                    swap(s2[i], s2[j]);
-                    possible = true;
-                    break;
-                }
-            }
-            if(!possible) return false;
+        //  first method
+        // for(int i=0;i<n;i++){
+        //     if(s1[i]==s2[i]) continue;
+        //     bool possible = false;
+        //     for(int j = i; j<n; j= j+2){
+        //         if(s1[i]==s2[j]){
+        //             swap(s2[i], s2[j]);
+        //             possible = true;
+        //             break;
+        //         }
+        //     }
+        //     if(!possible) return false;
+        // }
+
+        vector<vector<int>> mp(26, vector<int> (2, 0));
+        for(int i=0;i<n; i++){
+            mp[s1[i]-'a'][i%2]++;
+            mp[s2[i]-'a'][i%2]--;
+        }
+
+        for(int i=0;i<26; i++){
+            if(mp[i][0]!=0 ||  mp[i][1]!=0) return false;
         }
         return true;
     }

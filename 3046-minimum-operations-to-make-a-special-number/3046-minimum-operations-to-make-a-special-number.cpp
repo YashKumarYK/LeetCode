8ef0a2625dc91bb7c82384e91f68b37e07491315
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-09-05 16:14:38 +0530

    Time: 0 ms (100%), Space: 6.3 MB (28.29%) - LeetHub

--- 3046-minimum-operations-to-make-a-special-number/3046-minimum-operations-to-make-a-special-number.cpp
+++ 3046-minimum-operations-to-make-a-special-number/3046-minimum-operations-to-make-a-special-number.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-
-    bool cond(bool z , bool f , char ch){
-        if(z && ch=='0') return true;
-        if(z && ch=='5') return true;
-        if(f && ch=='7') return true;
-        if(f && ch=='2') return true;
-        return false;
-    }
     int minimumOperations(string num) {
         int n = num.size();
         bool zero = false;
         bool five = false;
 
         for(int i=n-1;i>=0;i--){
-            if(cond(zero, five, num[i])){
-                return n-2-i;
-            }
-
-            if(num[i]=='0') zero= true;
-            if(num[i]=='5') five = true;
+            char ch = num[i];
+            if(zero && (ch=='0' ||ch=='5'))  return n-2-i;
+            if(five && (ch=='7'|| ch=='2'))  return n-2-i;
+            if(ch=='0') zero= true;
+            if(ch=='5') five = true;
         }
-        if(zero==false){
-            return n;
-        }
-        else{
-            return n-1;
-        }
+        return zero?n-1: n;
     }
 };

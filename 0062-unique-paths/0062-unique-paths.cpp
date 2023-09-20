31808f6db330b68e3b3823afd323a8529867998b
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-09-20 09:33:25 +0530

    Time: 4 ms (6.62%), Space: 6.8 MB (10.3%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<int> curr(n+1, 0), prev(n+1, 0);
-        for(int i= 1;i<=m; i++){
-            for(int j = 1; j<=n; j++){
-                if(i==1 && j==1){
-                    curr[j] = 1;
-                    continue;
-                }
-                curr[j] = prev[j]+ curr[j-1];
-            }
-            prev = curr;
+    int findways(int m , int n, int i, int j, vector<vector<int>> &dp){
+        if(i==m-1 && j==n-1){ // at the end -> 1 ways
+             return 1; 
         }
-        return curr[n];
+        if(i>m || j> n) return 0; // outof bound - no ways
+
+        if(dp[i][j]!= -1) return dp[i][j];
+
+        int rightways  = findways(m, n, i, j+1, dp);
+        int downways =  findways(m, n, i+1, j, dp);
+
+        return dp[i][j] = rightways + downways;
+
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+
+        return findways(m , n, 0 ,0 ,dp);
     }
 };

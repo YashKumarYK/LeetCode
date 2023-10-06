471cf24b29cd4e024feb3776f3cd8758195ef3fc
Yash Kumar <97405524+YashKumarYK@users.noreply.github.com>
2023-10-06 22:51:14 +0530

    Time: 3 ms (30.5%), Space: 7 MB (6.83%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     
-    int solve( int i , vector<int> &num, int sum, vector<vector<int>> &dp){
-        if( i==num.size() || sum==0){
-            return 1;
-        }
-        if( dp[i][sum] !=-1) return dp[i][sum];
-        if( num[i]<=sum){
-            return dp[i][sum] = max( num[i]* solve( i, num, sum-num[i], dp), solve( i+1, num, sum, dp));
-        }
-        else{
-            return dp[i][sum] = solve(i+1, num, sum, dp);
-        }
+    int solve( int i , int sum, int n, vector<vector<int>> &dp){
+        if(sum == n) return 1;
+        if(sum>n || i==n) return -1;
+
+        if(dp[i][sum]!= -1) return dp[i][sum];
+        
+        int pick = i* solve(i, sum+i, n, dp);
+        int nopick = solve(i+1, sum, n, dp);
+        return dp[i][sum] = max(pick, nopick);
     }
     int integerBreak(int n) {
-        vector<int> num(n-1);
-        for(int i=0; i<n-1; i++){
-            num[i]= i+1;
-        }
-        vector<vector<int>> dp( n-1,vector<int> (n+1,-1));
-        return solve(0, num, n, dp);
+        vector<vector<int>> dp( n+1,vector<int> (n+1,-1));
+        return solve(1,0, n, dp);
     }
 };
